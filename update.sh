88df4bcdf761a7d5592d4103a1e0cd5e49b8ef20
Peter Johnson <insomnia@rcpt.at>
2016-12-01 19:34:04 +0100

    initial commit

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# sudo npm install -g togeojson
+mkdir -p kml geojson;
+
+URL="https://en.wikipedia.org/w/index.php";
+BASEURL="$URL?title=List_of_postcode_districts_in_the_United_Kingdom&printable=yes";
+
+wget -qO- $BASEURL | sed -n 's/.*href="\/wiki\/\([A-Z]*_postcode_area\).*/\1/p' | while read area;
+  do
+    echo "$area";
+    wget -qO- "$URL?title=Template:Attached_KML/$area&action=raw" > "kml/${area%_*_*}.kml";
+    togeojson "kml/${area%_*_*}.kml" > "geojson/${area%_*_*}.geojson";
+  done;
